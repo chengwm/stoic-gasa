4ed0cc4f2596e1164707bca4a7f113e4a1e660d3
Joel <bloodelves88@gmail.com>
2014-03-11 01:49:50 +0800

    Marcus stuff. Has shooting and enemies

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject bullethole;
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		RaycastHit hit;
+		
+		if(Input.GetMouseButtonDown(0))
+		{
+			if(Physics.Raycast(myRay,out hit)) {
+				Instantiate(bullethole, hit.point, Quaternion.identity);		
+				Debug.DrawRay(myRay.origin, myRay.direction*hit.distance, Color.red);
+
+				if(hit.transform.gameObject.tag == "Enemy") {
+					GameObject target = hit.collider.gameObject;
+					Enemy script = target.GetComponent<Enemy>();
+					script.StartAnim();
+				}
+			}
+				
+		}
+
+	}
+}
