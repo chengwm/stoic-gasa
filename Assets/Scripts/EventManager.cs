e230120dee81c0a5f21fdb8f9d790e30039ad3d1
chengwm <chengwm@localhost>
2014-03-25 21:46:39 +0800

    Main Hall and Dining Hall are up. Re-did Main Hall Some Standard Assets Removed Library not pushed

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+	
+	public Transform target1;
+	public Transform target2;
+	public float RotationSpeed;
+
+	//values for internal use
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
+
+	// Update is called once per frame
+	void Update () {
+		if ((!(GameObject.Find ("Target11"))) && (!(GameObject.Find ("Target12"))) && (!(GameObject.Find ("Target13")))) {
+
+			//find the vector pointing from our position to the target
+			_direction = (target1.position - transform.position).normalized;
+				
+			//create the rotation we need to be in to look at the target
+			_lookRotation = Quaternion.LookRotation(_direction);
+				
+			//rotate us over time according to speed until we are in the required rotation
+			transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+		}
+
+		if ((!(GameObject.Find ("Target21"))) && (!(GameObject.Find ("Target22"))) && (!(GameObject.Find ("Target23")))) {
+			//find the vector pointing from our position to the target
+			_direction = (target2.position - transform.position).normalized;
+			
+			//create the rotation we need to be in to look at the target
+			_lookRotation = Quaternion.LookRotation(_direction);
+			
+			//rotate us over time according to speed until we are in the required rotation
+			transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+
+		}
+		if ((!(GameObject.Find ("Target31"))) && (!(GameObject.Find ("Target32"))) && (!(GameObject.Find ("Target33")))) {
+						Application.LoadLevel ("endofdemo");
+				}
+	}
+}
---
+++ Assets/Scripts/VolumeButtons.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeButtons : MonoBehaviour {
+
+	public float effectsVolume = 5.0F;
+	public float musicVolume = 5.0F;
+	
+	public Texture2D button1;
+	public Texture2D button2;
+	
+	public TextMesh musicVolumeText;
+	public TextMesh effectsVolumeText;
+
+	void Start(){
+		if (PlayerPrefs.HasKey ("effectsVolume")) {
+			effectsVolume = PlayerPrefs.GetInt ("effectsVolume");
+		}
+		if (PlayerPrefs.HasKey ("musicVolume")) {
+			musicVolume = PlayerPrefs.GetInt ("musicVolume");
+		}
+	}
+
+	void OnMouseUp(){
+		if (this.name == "MusicUp"){
+			guiTexture.texture = button1;
+			if(musicVolume < 10)
+			{
+				musicVolume+=1;
+				PlayerPrefs.SetInt ("musicVolume", (int)musicVolume);
+			}
+		}
+		else if (this.name == "MusicDown"){
+			guiTexture.texture = button1;
+			if(musicVolume > 0)
+			{
+				musicVolume-=1;
+				PlayerPrefs.SetInt ("musicVolume", (int)musicVolume);
+			}
+		}
+		else if (this.name == "EffectsUp"){
+			guiTexture.texture = button1;
+			if(effectsVolume < 10)
+			{	
+				effectsVolume+=1;
+				PlayerPrefs.SetInt ("effectsVolume", (int)effectsVolume);
+			}
+		}
+		else if (this.name == "EffectsDown"){
+			guiTexture.texture = button1;
+			if(effectsVolume > 0)
+			{
+				effectsVolume-=1;
+				PlayerPrefs.SetInt ("effectsVolume", (int)effectsVolume);
+			}
+		}
+	}
+	
+	void OnMouseDown(){
+		if (guiTexture.name == "MusicUp")
+			guiTexture.texture = button2;
+		else if (guiTexture.name == "MusicDown")
+			guiTexture.texture = button2;
+		else if (guiTexture.name == "EffectsUp")
+			guiTexture.texture = button2;
+		else if (guiTexture.name == "EffectsDown")
+			guiTexture.texture = button2;
+	}
+	
+	void Update()
+	{
+		if (PlayerPrefs.HasKey ("effectsVolume")) {
+			effectsVolume = PlayerPrefs.GetInt ("effectsVolume");
+		}
+		if (PlayerPrefs.HasKey ("musicVolume")) {
+			musicVolume = PlayerPrefs.GetInt ("musicVolume");
+		}
+	
+		musicVolumeText.text = musicVolume.ToString ();
+		effectsVolumeText.text = effectsVolume.ToString ();
+	}
+}
