d567f34ae6df1557fe416de9b73dbb81722c8131
Joel Ng <bloodelves88@gmail.com>
2014-04-02 16:23:05 +0800

    - Headshot detection - Highscore: Gummybear body = 10 points, head = 20 points, lollipop = 20 points, egg = 30 points - Bugfix: Cannot shoot shield

---
+++ Assets/Scripts/InGameScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameScoreScript : MonoBehaviour {
+
+	float score = 0;
+	public int currentScore = 0;
+	public int highScore;
+	//public LifeCounter lifecounterScript;
+
+	void Start(){
+		if (PlayerPrefs.HasKey ("highScore")) {
+			highScore = PlayerPrefs.GetInt ("highScore");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		guiText.text = "Score: " + currentScore.ToString("F0");
+	}
+}
