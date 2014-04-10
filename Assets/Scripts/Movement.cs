ea330b7cce424d056127409595f9481763a94adb
Kun Tat <kuntat@gmail.com>
2014-04-10 15:41:28 +0800

    added more waypoints

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,32 +3,90 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-	public int arraySize = 0;
-	public GameObject[] waypoints;
+	public int listSize, routeNo, routeWaypoint, arraySize, whichRoute;
+	public GameObject[] list_waypoints;
+	public GameObject[][] route_waypoints;
+	public GameObject[] minion_waypoints;
+
 	private int i = 0;
 	private Vector3 wPos, ePos;
 	public bool reached = false;
 	public int pass = 1;
-	private bool updateSize = true;
+
+	private bool doInitialise = true;
 
 	// Use this for initialization
 	void Start ()
 	{
-		// waypoints = new GameObject[arraySize];
+		//set up the list of all the waypoints
+
+		listSize = 11;
+		list_waypoints = new GameObject[listSize];
+		list_waypoints[0] = GameObject.Find("waypoint0");
+		list_waypoints[1] = GameObject.Find("waypoint1");
+		list_waypoints[2] = GameObject.Find("waypoint2");
+		list_waypoints[3] = GameObject.Find("waypoint3");
+		list_waypoints[4] = GameObject.Find("waypoint4");
+		list_waypoints[5] = GameObject.Find("waypoint5");
+		list_waypoints[6] = GameObject.Find("waypoint6");
+		list_waypoints[7] = GameObject.Find("waypoint7");
+		list_waypoints[8] = GameObject.Find("waypoint8");
+		list_waypoints[9] = GameObject.Find("waypoint9");
+		//last node is always Main Camera
+		list_waypoints[listSize-1] = GameObject.Find("Main Camera");
+
+		//organise waypoints into routes
+		routeNo = 3;
+		route_waypoints = new GameObject[routeNo][];
+
+		//route 0
+		routeWaypoint = 5;
+		route_waypoints[0] = new GameObject[routeWaypoint];
+		route_waypoints[0][0] = list_waypoints[0];
+		route_waypoints[0][1] = list_waypoints[1];
+		route_waypoints[0][2] = list_waypoints[2];
+		route_waypoints[0][3] = list_waypoints[3];
+		route_waypoints[0][4] = list_waypoints[listSize-1];
+
+		//route 1
+		routeWaypoint = 6;
+		route_waypoints[1] = new GameObject[routeWaypoint];
+		route_waypoints[1][0] = list_waypoints[0];
+		route_waypoints[1][1] = list_waypoints[1];
+		route_waypoints[1][2] = list_waypoints[2];
+		route_waypoints[1][3] = list_waypoints[3];
+		route_waypoints[1][4] = list_waypoints[4];
+		route_waypoints[1][5] = list_waypoints[listSize-1];
+
+		//route 2
+		routeWaypoint = 6;
+		route_waypoints[2] = new GameObject[routeWaypoint];
+		route_waypoints[2][0] = list_waypoints[5];
+		route_waypoints[2][1] = list_waypoints[6];
+		route_waypoints[2][2] = list_waypoints[7];
+		route_waypoints[2][3] = list_waypoints[8];
+		route_waypoints[2][4] = list_waypoints[9];
+		route_waypoints[2][5] = list_waypoints[listSize-1];
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		if(updateSize == true && arraySize != 0)
+		if(doInitialise == true && arraySize != 0)
 		{
-			waypoints = new GameObject[arraySize];
-			waypoints[0] = GameObject.Find("waypoint11");
-			waypoints[1] = GameObject.Find("waypoint12");
-			waypoints[2] = GameObject.Find("waypoint13");
-			waypoints[3] = GameObject.Find("waypoint14");
-			waypoints[4] = GameObject.Find("waypoint15");
-			updateSize = false;
+			minion_waypoints = new GameObject[arraySize];
+			/*
+			minion_waypoints[0] = GameObject.Find("waypoint11");
+			minion_waypoints[1] = GameObject.Find("waypoint12");
+			minion_waypoints[2] = GameObject.Find("waypoint13");
+			minion_waypoints[3] = GameObject.Find("waypoint14");
+			minion_waypoints[4] = GameObject.Find("waypoint15");
+			*/
+			//for(int i = 0; i < arraySize; i++)
+			//{
+				minion_waypoints = route_waypoints[whichRoute];
+			//}
+			doInitialise = false;
 			print ("array created");
 		}
 		if(i == arraySize-1)
@@ -46,7 +104,7 @@ public class Movement : MonoBehaviour
 		else
 		{
 			//print ("name = " + this.name + " and i = "+ i + " and reached = " + reached);
-			wPos = waypoints[i].transform.position;
+			wPos = minion_waypoints[i].transform.position;
 			//print ("wPos = " + wPos);
 			ePos = transform.position;
 			//if(i == arraySize-2)
@@ -57,7 +115,7 @@ public class Movement : MonoBehaviour
 				//rigidbody.velocity = ((wPos - ePos) / (wPos - ePos).magnitude) * 30.0f;
 			rigidbody.velocity = (wPos - ePos).normalized * 30.0f;
 
-			Vector3 direction = (waypoints[i].transform.position - transform.position).normalized;
+			Vector3 direction = (minion_waypoints[i].transform.position - transform.position).normalized;
 			float temp = -direction.z;
 			direction.z = temp;
 			Quaternion lookRotation = Quaternion.LookRotation(direction);
