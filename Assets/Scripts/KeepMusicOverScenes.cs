b4c8c61aabee3bac4289bb0dca769b8d08b38e50
Kun Tat <kuntat@gmail.com>
2014-04-14 00:49:36 +0800

    Modified to fit in spawning according to waves

---
+++ Assets/Scripts/KeepMusicOverScenes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepMusicOverScenes : MonoBehaviour {
+
+	private static KeepMusicOverScenes instance = null;
+	public static KeepMusicOverScenes Instance{
+		get { return instance; }
+	}
+	public AudioClip gameMusic;
+	
+	void Start(){
+		audio.clip = gameMusic;
+		if(Application.loadedLevelName == "mainHall"){
+			audio.Play ();
+		}
+		
+		if(instance != null && instance != this){
+			Destroy (this.gameObject);
+			return;
+		}
+		else{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
+
