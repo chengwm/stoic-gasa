4045712a42c11a9f5db1a0a7cb75a70343946648
Joel Ng <bloodelves88@gmail.com>
2014-04-15 14:46:18 +0800

    -This is the submission to Samsung -Disabled FPS counter

--- Assets/Scripts/LifeCounter.cs
+++ Assets/Scripts/LifeCounter.cs
@@ -50,6 +50,7 @@ public class LifeCounter : MonoBehaviour {
 		if (PlayerPrefs.HasKey ("playerHealth")) {
 			playerHealth = PlayerPrefs.GetInt ("playerHealth");
 		}
+		// playerHealth = 3; // god mode
 		// If player has 4 lives
 		if(playerHealth >= 4)
 		{
